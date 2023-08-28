9ec359fbe6c7bf9f8b1a3caf26be8af086caaaa8
Fahima <sfahima1388@gmail.com>
2023-08-28 08:50:11 +0600

    added phone cards

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const loadPhone = () => {
   fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => displayPhones(data.data));
 };
-
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+    phoneCard.innerHTML = `
+    <figure class="px-10 pt-10">
+    <img src="${phone.image} " alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">${phone.phone_name}  </h2>
+    <p>There are many variations of passages of available, but the majority have suffered</p>
+    <div class="card-actions">
+<button class="btn btn-primary">Show Details</button>
+    </div>
+  </div>
+    `;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
 loadPhone();
