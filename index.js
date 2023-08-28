daad324f5194962f5b9951bab1da113d1e778d70
Fahima <sfahima1388@gmail.com>
2023-08-28 21:12:09 +0600

    button

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const loadPhone = () => {
-  fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+const loadPhone = (searchText) => {
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then((res) => res.json())
     .then((data) => displayPhones(data.data));
 };
 const displayPhones = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
+
+  //clear search bar
+  phoneContainer.textContent = "";
   phones.forEach((phone) => {
     console.log(phone);
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+    phoneCard.classList = `card bg-base-100 shadow-xl`;
     phoneCard.innerHTML = `
     <figure class="px-10 pt-10">
     <img src="${phone.image} " alt="Shoes" class="rounded-xl" />
@@ -17,11 +20,18 @@ const displayPhones = (phones) => {
     <h2 class="card-title">${phone.phone_name}  </h2>
     <p>There are many variations of passages of available, but the majority have suffered</p>
     <div class="card-actions">
-<button class="btn btn-primary">Show Details</button>
+<button class="btn btn-accent">Show Details</button>
     </div>
   </div>
     `;
     phoneContainer.appendChild(phoneCard);
   });
 };
-loadPhone();
+
+//handle search button
+const handleSearch = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText);
+};
